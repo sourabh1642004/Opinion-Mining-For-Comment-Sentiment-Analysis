5da3dd6c8b15833b1334890c942b547cf12844b0
omagr2004 <162777001+omagr2004@users.noreply.github.com>
2024-09-22 19:27:54 +0530

    Phase 1

--- omcsa/omcsa/views.py
+++ omcsa/omcsa/views.py
@@ -4,11 +4,20 @@ from django.utils import timezone
 from datetime import timedelta
 from django.shortcuts import render
 from django.shortcuts import render, redirect
+from textblob import TextBlob
 
 def index(request):
+    sentiment = None
     if request.method == 'POST':
         comment = request.POST.get('comment')
         with open('comments.txt', 'a') as file:
             file.write(comment + '\n')
-        return redirect('index')
+        analysis = TextBlob(comment)
+        if analysis.sentiment.polarity > 0:
+            sentiment = 'Positive'
+        elif analysis.sentiment.polarity < 0:
+            sentiment = 'Negative'
+        else:
+            sentiment = 'Neutral'
+        return render(request, "index.html", {'sentiment': sentiment})
     return render(request, "index.html")
