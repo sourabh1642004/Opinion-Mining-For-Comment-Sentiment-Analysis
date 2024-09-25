0aa8b114e8b97e259b491c51cbcaebd41228e9f0
omagr2004 <162777001+omagr2004@users.noreply.github.com>
2024-09-25 15:37:07 +0530

    phase 2

--- omcsa/omcsa/views.py
+++ omcsa/omcsa/views.py
@@ -1,23 +1,43 @@
-from django.http import HttpResponse
-from django.http import HttpResponse,HttpResponseRedirect
+from django.http import HttpResponse, HttpResponseRedirect
 from django.utils import timezone
 from datetime import timedelta
-from django.shortcuts import render
 from django.shortcuts import render, redirect
 from textblob import TextBlob
+from langdetect import detect
+from googletrans import Translator
+import pycountry
 
+def get_language_name(lang_code):
+    try:
+        # Handle locale codes like 'zh-cn'
+        if '-' in lang_code:
+            lang_code = lang_code.split('-')[0]
+        return pycountry.languages.get(alpha_2=lang_code).name
+    except AttributeError:
+        return lang_code
 def index(request):
-    sentiment = None
+    sentiments = []
+    translator = Translator()
     if request.method == 'POST':
-        comment = request.POST.get('comment')
-        with open('comments.txt', 'a') as file:
-            file.write(comment + '\n')
-        analysis = TextBlob(comment)
-        if analysis.sentiment.polarity > 0:
-            sentiment = 'Positive'
-        elif analysis.sentiment.polarity < 0:
-            sentiment = 'Negative'
+        if 'file' in request.FILES:
+            file = request.FILES['file']
+            comments = file.read().decode('utf-8').splitlines()
         else:
-            sentiment = 'Neutral'
-        return render(request, "index.html", {'sentiment': sentiment})
-    return render(request, "index.html")
+            comments = [request.POST.get('comment')]
+
+        for comment in comments:
+            lang_code = detect(comment)
+            lang_name = get_language_name(lang_code)
+            original_comment = comment
+            if lang_code != 'en':
+                comment = translator.translate(comment, src=lang_code, dest='en').text
+            analysis = TextBlob(comment)
+            if analysis.sentiment.polarity > 0:
+                sentiment = 'Positive'
+            elif analysis.sentiment.polarity < 0:
+                sentiment = 'Negative'
+            else:
+                sentiment = 'Neutral'
+            sentiments.append((original_comment, lang_name, sentiment))
+
+    return render(request, 'index.html', {'sentiments': sentiments})
